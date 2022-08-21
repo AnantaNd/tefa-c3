1e99092ca94a6b776f59b42496a6696dae02dc62
AnantaNd <ananta.nov2@gmail.com>
2022-08-21 21:13:28 +0700

    make responsive

--- components/Header.js
+++ components/Header.js
@@ -5,19 +5,20 @@ import {FaBars} from 'react-icons/fa'
 
 export default function Header (){
   const [isOpen, setIsOpen] = useState(false)
-  const navOpen = () => setIsOpen(!setIsOpen)
+  const navOpen = () => setIsOpen(!isOpen)
 
 
   return (
-    <div className={style.container}>
-      <nav>
-        <ul className={style.menu}>
-          <li className={style.logo}><a href="#/">Hunger App</a></li>
-          <li className={style.menuItem}><Link href="#about">About</Link></li>
-          <li className={style.menuItem}><Link href="#Favorite">Favorite</Link></li>
-          <li className={style.menuItem}><Link className="" href="#home">Home</Link></li>
-        </ul>
-      </nav>
-    </div>
+    <nav className={style.navbar}>
+      <div className={style.logo}><a href="#/">Hunger App</a></div>
+      <ul className={!isOpen ? style.menu : style.menures}>
+        <li className={style.item}><Link className="" href="#home">Home</Link></li>
+        <li className={style.item}><Link href="#Favorite">Favorite</Link></li>
+        <li className={style.item}><Link href="#about">About</Link></li>
+      </ul>
+      <div className={style.burger} onClick={navOpen}>
+        <FaBars/>
+      </div>
+    </nav>
   )
 }
