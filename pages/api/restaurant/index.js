3847db36f4425258bf91f5dd294c2ce2c7ac26aa
AnantaNd <ananta.nov2@gmail.com>
2022-08-27 22:57:56 +0700

    fix font styling

--- pages/api/restaurant/index.js
+++ pages/api/restaurant/index.js
@@ -1,28 +1,29 @@
 import {restaurant} from '../../../data.js'
 
 export default function handler(req, res){
-  const httpMethod = req.method
+  const httpMethod = req.method;
   const { title, rating, location, description, image } = req.body;
 
   switch(httpMethod){
-    // show all data
     case 'GET':
+      // show all data
       res.status(200).json(restaurant);
       break;
-    // create new restaurant
-    case 'POST':
-      restaurant.push({
-        id: restaurant.length+1,
-        title: title,
-        rating: rating,
-        location: location,
-        description: description,
-        image: image
-      })
-      res.status(200).json({restaurant});
-      break;
+      case 'POST':
+      // create new restaurant
+      const newRest = { 
+        id: restaurant.length + 1,
+        title,
+        rating,
+        location,
+        image,
+        description
+      }
+      restaurant.push(newRest)
+      res.status(200).json(restaurant);      
     default:
       res.setHeader('allow', ['GET', 'POST']);
       res.status(405).end(`method ${httpMethod} not allowed`);
+      break;
   }
 }
