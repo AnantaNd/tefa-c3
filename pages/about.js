f1f0ff27b677954bfd9597ad512cf712caad2322
AnantaNd <ananta.nov2@gmail.com>
2022-09-20 20:55:07 +0700

    fix styling about.js

--- pages/about.js
+++ pages/about.js
@@ -17,19 +17,22 @@ export default function About(){
     <>
       <Navbar/>
       <div className={style.container}>
-        <h1 className={style.title}>about us</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input className={style.input_field} {...register("firstName")} placeholder="name" />
-          <select className={style.input_field} {...register("gender")}>
-            <option value="">select gender</option>
-            <option value="female">female</option>
-            <option value="male">male</option>
-          </select>
-          <button className={style.btn_submit} type='submit'>submit</button>
-        </form>
-        <div className={style.display}>
-          <p className={style.display_name}>{isData.firstName}</p>
-          <p className={style.display_gender}>{isData.gender}</p>
+        <div className={style.about}>
+          <h1 className={style.title}>about us</h1>
+          <p className={style.desc_about}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        </div>
+        <div className={style.comment}>
+          <h1 className={style.title_comment}>comment</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input className={style.input_email} {...register("email")} placeholder="email" />
+            <textarea className={style.input_comment} {...register("comment")} placeholder="comment ..." />
+            <button className={style.btn_submit} type='submit'>submit</button>
+          </form>
+        </div>
+        <div className={style.display_comment}>
+          <h1 className={style.title}>comment</h1>
+          <p>{isData.email}</p>
+          <p>{isData.comment}</p>
         </div>
       </div>
     </>
